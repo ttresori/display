7fe1f7074d69bf6a1dc66cbdc4ca5dda357b34c7
Chunky <chunky@mac.home>
2025-06-20 20:28:21 +0200

    init i2c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "lib.h"
+
+static void init_i2c_pin() {
+  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+}
+
+static void init_pull_up_i2c() {
+  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+}
+
 int main() {
+  stdio_init_all();
+  i2c_init(i2c_default, SSD1306_I2C_CLK * 1000);
+  init_i2c_pin();
+  init_pull_up_i2c();
   return 1;
 }
